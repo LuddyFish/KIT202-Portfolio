754a7edd4e81dd11f3729a8e13913750a370a16a
LuddyFish <jackjumper.l03@gmail.com>
2025-03-25 14:00:21 +1100

    Wk5: Finished Activity 2

--- js_intro/js/playground.js
+++ js_intro/js/playground.js
@@ -5,6 +5,20 @@ console.log("playground.js is running");
 
 /* Activity 2: Football Trophy */
 
+let maskedOwlsWin = `Team Masked Owls wins the trophy!`;
+let quollsWin = `Team Quolls wins the trophy!`;
+let draw = `It's a draw! Both teams share the trophy`;
+
+function winner(maskedOwlsResults, quollsResults) {
+    if (maskedOwlsResults > quollsResults) {
+        return maskedOwlsWin;
+    } else if (maskedOwlsResults < quollsResults) {
+        return quollsWin;
+    } else {
+        return draw;
+    }
+}
+
 // Activity 2, Rule 1
 let maskedOwls = [72, 63, 99, 105];
 let quolls = [44, 89, 92, 111];
@@ -20,33 +34,36 @@ function average(values) {
 }
 
 function determineRule1Winner() {
-    //TODO Write your code for determining the winner of the two teams and updating the page here
-    owl_avg = average(maskedOwls);
-    quoll_avg = average(quolls);
+    let owl_avg = average(maskedOwls);
+    let quoll_avg = average(quolls);
 
-    if (owl_avg > quoll_avg) {
-        console.log(`Team Masked Owls wins the trophy!`)
-    } else if (quoll_avg > owl_avg) {
-        console.log(`Team Quolls wins the trophy!`)
-    } else {
-        console.log(`It's a draw! Both teams share the trophy`)
-    }
+    return winner(owl_avg, quoll_avg);
 }
 
 // Activity 2, Rule 2
+let newMaskedOwls = [86, 45, 54, 73, 124];
+let newQuolls = [78, 61, 56, 77, 101];
 
 function wins(scores1, scores2) {
-    //TODO Implement this function
+    let wins = 0;
+    for (let i in scores1) {
+        if (scores1[i] > scores2[i]) {
+            wins++;
+        }
+    }
+
+    return wins;
 }
 
 function determineRule2Winner() {
-    //TODO Write your code for determining the winner of the two teams under the new conditions and updating the page here
+    let owl_wins = wins(newMaskedOwls, newQuolls);
+    let quoll_wins = wins(newQuolls, newMaskedOwls);
+
+    return winner(owl_wins, quoll_wins);
 }
 
-//TODO Call the top-level functions determineRule1Winner and determinRule2Winner when they're ready
-
-
-
+document.getElementById("activity2-highest-avg").innerHTML = determineRule1Winner();
+document.getElementById("activity2-most-wins").innerHTML =determineRule2Winner();
 
 /* Activity 3: Tax */
 
