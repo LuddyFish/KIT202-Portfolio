3af2d9e06ea4710cc2f7c1c87bbb9c268bf0b87d
jackl3 <jackl3@utas.edu.au>
2025-05-16 13:37:19 +1000

    updated wk11 to pt.2

--- dynamic_site/add.php
+++ dynamic_site/add.php
@@ -1,4 +1,16 @@
 <?php
 
-// Write your PHP code here
+include "dbconn.php";
 
+$new = "INSERT 
+        INTO Participant (firstName, lastName, DOB, gender, experience)
+        VALUES (?, ?, ?, ?, ?)";
+
+$statement = $conn->prepare($new);
+$statement->bind_param('sssss', $_POST['fname'], $_POST['lname'], $_POST['dob'], $_POST['gender'], $_POST['experienced']);
+
+if ($statement->execute()) {
+    header("Location: participants.php");
+}
+
+$conn->close();
